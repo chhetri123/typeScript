b1f5c45e42314a158793280110ee4977d03b51b1
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2021-10-24 19:31:15 +0545

    Chapter-3

--- Next-generation JavaScript & TypeScript/src/app.ts
+++ Next-generation JavaScript & TypeScript/src/app.ts
@@ -1,34 +1,34 @@
-// const userName = 'Max';
-// // userName = 'Maximilian';
-// let age = 30;
+const userName = 'Max';
+// userName = 'Maximilian';
+let age = 30;
 
-// age = 29;
+age = 29;
 
-// function add(a: number, b: number) {
-//   let result;
-//   result = a + b;
-//   return result;
-// }
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
 
-// if (age > 20) {
-//   let isOld = true;
-// }
+if (age > 20) {
+  let isOld = true;
+}
 
-// console.log(isOld);
+console.log(isOld);
 
-// console.log(result);
+console.log(result);
 
-// const add = (a: number, b: number = 1) => a + b;
+const add = (a: number, b: number = 1) => a + b;
 
-// const printOutput: (a: number | string) => void = output => console.log(output);
+const printOutput: (a: number | string) => void = output => console.log(output);
 
-// const button = document.querySelector('button');
+const button = document.querySelector('button');
 
-// if (button) {
-//   button.addEventListener('click', event => console.log(event));
-// }
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
 
-// printOutput(add(5));
+printOutput(add(5));
 
 const hobbies = ["Sports", "Cooking"];
 const activeHobbies = ["Hiking"];
